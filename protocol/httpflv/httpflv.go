543a6a61ee7c05469e94c7f2aa7c171e81a6083f
ubinte <ubinte@gmail.com>
2020-05-27 20:42:25 +0800

    fix httpflv

--- protocol/httpflv/httpflv.go
+++ protocol/httpflv/httpflv.go
@@ -7,7 +7,7 @@ import (
 )
 
 func HandleWriter(app, channel, url string, handler av.Handler, w http.ResponseWriter) {
-	flvWriter := NewFLVWriter("live", "movie", url, w)
+	flvWriter := NewFLVWriter(app, channel, url, w)
 	handler.HandleWriter(flvWriter)
 	flvWriter.Wait()
 }
